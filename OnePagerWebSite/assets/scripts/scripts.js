ac802d7fa1a4fae5a3cf596ba65cbf17c040d5bd
Bhumika <bhumika04shimla@gmail.com>
2025-07-24 12:22:19 +0530

    wrote full javascript

--- OnePagerWebSite/assets/scripts/scripts.js
+++ OnePagerWebSite/assets/scripts/scripts.js
@@ -71,6 +71,15 @@
 //     })
 // }
 
+$(document).ready(function() {
+    initNavigation();
+
+    $('.slides-container').slick({
+        arrows: true,
+        dots: true,
+        autoplay: true
+    })
+})
 function initNavigation() {
     //declaration and init of variables
     var navToggle = $('.nav-icon');
@@ -97,15 +106,41 @@ function initNavigation() {
 
         $('html, body').animate({
             scrollTop: parseInt(targetElement.offset().top)
-        }, animationDuration, swing);
+        }, animationDuration, 'swing');
     })
     $('.back-to-top, .logo-link').click(function(e) {
         e.preventDefault();
         var animationDuration = 1000;
 
         $('html, body').animate({
-            scrollTop: parseInt(targetElement.offset().top)
-        }, animationDuration, swing);
+            scrollTop: 0
+        }, animationDuration, 'swing');
         
+    })
+
+    $(document).scroll(function() {
+        var currentScroll = $(document).scrollTop();
+
+        var scrollItems = anchorLinks.map(function() {
+            var item = $($(this).attr('href'));
+
+            if(item.length) {
+                return item;
+            }
+        })
+
+        var currentLink = scrollItems.map(function(){
+            if(parseInt($(this).offset().top) <= currentScroll) {
+                return this;
+            }
+        })
+
+        currentLink = currentLink[currentLink.length -1];
+        var id = currentLink && currentLink.length ? currentLink[0].id :"";
+
+        anchorLinks.filter("[href! = '#"+id+"']").removeClass('active');
+        navToggle.removeClass('active');
+        mainNav.removeClass('active');
+        anchorLinks.filter("[href! = '#"+id+"']").addClass('active');
     })
 }
